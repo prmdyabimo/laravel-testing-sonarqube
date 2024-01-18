8099e56b3118947f8c9f1939da16bed35fc7f4fd
prmdyabimo <pramudyabimo1920@gmail.com>
2024-01-18 12:51:43 +0700

    add sql injection

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -148,3 +148,17 @@
 </body>
 
 </html>
+
+<?php
+
+$id = $_GET['id'];
+mysql_connect('localhost', $username, $password) or die('Could not connect: ' . mysql_error());
+mysql_select_db('myDatabase') or die('Could not select database');
+
+$result = mysql_query('SELECT * FROM myTable WHERE id = ' . $id); // Sensitive, could be susceptible to SQL injection
+
+while ($row = mysql_fetch_object($result)) {
+    echo $row->name;
+}
+
+?>
